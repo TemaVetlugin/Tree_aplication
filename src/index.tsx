1cb5655cd13b581620ddbc2a2d0040747bda2c40
TemaVetlugin <artem34349@gmail.com>
2023-08-01 16:15:42 +0700

    cosmetic changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Tree from './components/Tree';
-import ModalAdd from './components/ModalAdd';
 import reportWebVitals from './reportWebVitals';
 
 
